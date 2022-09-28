77a83272c677e478a393046cbfbd0c53fd02736e
ketanGangal <ketangangal98@gmail.com>
2022-09-28 15:33:14 +0530

    added fastapi for async calls

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-import os, sys
-from flask import Flask,send_file, abort, render_template, request,url_for,flash,redirect
+from fastapi import FastAPI, Request
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import File, UploadFile
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"))
+TEMPLATES = Jinja2Templates(directory='templates')
 
 
-app=Flask(__name__)
+@app.get("/", status_code=200)
+@app.post("/")
+async def index(request: Request):
+    return TEMPLATES.TemplateResponse(name='index.html', context={"request": request})
 
-@app.route("/",methods=['GET','POST'])
-def index():
-    return render_template('index.html')
 
-@app.route('/image', methods=['GET', 'POST'])
-def upload_file():
-    print(request.files)
-    print(request.method)
-    
-    return ""
+@app.post('/image')
+def upload_file(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
 
-if __name__=="__main__":
-    app.run(debug=True)
+    return {"message": f"Successfully uploaded {file.filename}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
---
+++ estimator/components/predict.py
@@ -0,0 +1,3 @@
+# Enable Bucket Versioning
+# As soon as we get bucket event trigger -> pull the image intp
+# Load Model From Models
